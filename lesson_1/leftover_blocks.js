d5bb777bb5ffe8c9ccd9564ec547cab0008ea39f
Darren Churchill <darrenmchurchill@gmail.com>
2023-03-06 18:11:30 -0800

    Add leftover_blocks.js

---
+++ lesson_1/leftover_blocks.js
@@ -0,0 +1,39 @@
+/**
+ * You have a number of building blocks that can be used to build a valid
+ * structure.  There are certain rules about what determines a structure's
+ * validity.
+ *
+ * - The building blocks are cubes
+ * - The structure is built in layers
+ * - The top layer is a single block
+ * - A block in an upper layer must be supported by four blocks in a lower layer
+ * - A block in a lower layer can support more than one block in an upper layer
+ * - You cannot leave gaps between blocks
+ *
+ * Write a program that, given a specific number of cubes, calculates the number
+ * of blocks left over after building the tallest possible structure.
+ */
+
+function calculateLeftoverBlocks(numBlocks) {
+  let curLayer = 1;
+  let numRemaining = numBlocks;
+  let blocksNeeded = curLayer ** 2;
+
+  while (blocksNeeded <= numRemaining) {
+    numRemaining -= blocksNeeded;
+    curLayer += 1;
+    blocksNeeded = curLayer ** 2;
+  }
+
+  return numRemaining;
+}
+
+
+// Test cases:
+console.log(calculateLeftoverBlocks(0) === 0); //true
+console.log(calculateLeftoverBlocks(1) === 0); //true
+console.log(calculateLeftoverBlocks(2) === 1); //true
+console.log(calculateLeftoverBlocks(4) === 3); //true
+console.log(calculateLeftoverBlocks(5) === 0); //true
+console.log(calculateLeftoverBlocks(6) === 1); //true
+console.log(calculateLeftoverBlocks(14) === 0); //true
